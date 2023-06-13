8afae37803edb5c307dffac5e5e880dc40eefc05
Matheus Mansano <matheusmansano1@gmail.com>
2023-06-13 18:10:28 -0300

    atualizações

--- script.js
+++ script.js
@@ -16,49 +16,6 @@ window.addEventListener('scroll', function(){
     mon2.style.marginLeft = value / '35.0' + '%';
 })
 
-//função magnética dos projetos
-login.addEventListener('mousemove', (e) => {
-    let x = e.offsetX;
-    let y = e.offsetY;
-    let loginWidth = login.clientWidth;
-    let loginHeight = login.clientHeight;
-    let moveX = (x - loginWidth/2);
-    let moveY = (y - loginHeight/2);
-    login.style.transform = `translateX(${moveX}px) translateY(${moveY}px)`;
-})
-
-login.addEventListener('mouseout', (e) => {
-    login.style.transform = ``;
-})
-
-cordel.addEventListener('mousemove', (e) => {
-    let x = e.offsetX;
-    let y = e.offsetY;
-    let cordelWidth = cordel.clientWidth;
-    let cordelHeight = cordel.clientHeight;
-    let moveX = (x - cordelWidth/2);
-    let moveY = (y - cordelHeight/2);
-    cordel.style.transform = `translateX(${moveX}px) translateY(${moveY}px)`;
-})
-
-cordel.addEventListener('mouseout', (e) => {
-    cordel.style.transform = ``;
-})
-
-form.addEventListener('mousemove', (e) => {
-    let x = e.offsetX;
-    let y = e.offsetY;
-    let formWidth = form.clientWidth;
-    let formHeight = form.clientHeight;
-    let moveX = (x - formWidth/2);
-    let moveY = (y - formHeight/2);
-    form.style.transform = `translateX(${moveX}px) translateY(${moveY}px)`;
-})
-
-form.addEventListener('mouseout', (e) => {
-    form.style.transform = ``;
-})
-
 //função do hambúrguer
 function clickMenu(){
     if ( itens.style.display == 'block') {
